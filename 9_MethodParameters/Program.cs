59c909c1d6f0d9228c79ab7d03e1c52688e0b97a
Sushant Thakare <148753657+isushantthakare@users.noreply.github.com>
2024-04-26 16:26:02 +0530

    Method Parameters

---
+++ 9_MethodParameters/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_MethodParameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 10;
+            //PrintA(a);
+            //Console.WriteLine(a);
+
+            int b = 10;
+            PrintB(ref b);
+            Console.WriteLine(b);
+
+           int c ;
+            PrintC(out c);
+            Console.WriteLine(c);
+
+            int a = 40, bc = 20, add, sub, mul, div;
+            PrintD( a,bc,out add,out sub,out mul,out div);  
+            Console.WriteLine($"addition ={add}");
+            Console.WriteLine($"Substraction ={sub}");
+            Console.WriteLine($"Multiplication ={mul}");
+            Console.WriteLine($"Division ={div}");
+            Console.ReadLine();
+        }
+
+        //public static void PrintA(int a)
+        //{
+        //    a = 100;
+        //}
+        public static void PrintB(ref int b)
+        {
+         b= 100;
+        }
+        public static void PrintC(out int c)
+        {
+          c= 100;
+        }
+
+
+        public static void PrintD( int a,int b,out int add,out int sub,out int multi,out int div)
+        {
+            add = a + b;    
+            sub = a - b;    
+            multi = a * b;    
+            div = a / b;    
+        }
+
+    }
+}
